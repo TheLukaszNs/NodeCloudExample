61e02d858d0a0f5985ac1245eb5b77d30bc89b74
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2018-12-09 20:01:22 +0100

    Initial Example Commit

---
+++ .nc.config.js
@@ -0,0 +1,14 @@
+const providers = [
+  {
+    name: 'google',
+    tag: 'google',
+    libName: 'nodecloud-gcp-plugin',
+    configFile: {
+      keyFilename:
+        'E:\\Lukasz\\Programming\\GCI\\nodecloud-gcp-dry-run\\key.js',
+      projectId: 'dronesym-thelukaszns',
+    },
+  },
+];
+
+module.exports = providers;
---
+++ index.js
@@ -0,0 +1,9 @@
+const nodeCloud = require('nodecloud');
+const gcpProvider = nodeCloud.getProvider(
+  'google',
+  'E:\\Lukasz\\Programming\\GCI\\nodecloud-gcp-dry-run',
+);
+
+// const googleSDK = gcpProvider.getSDK()
+
+console.log(gcpProvider);
